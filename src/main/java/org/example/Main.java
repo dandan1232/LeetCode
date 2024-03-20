d3d52d6c7e48eb1ec55c8930e51a06dafdd16025
dandan1232 <345652127@qq.com>
2024-03-20 09:55:54 +0800

    起项目

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author: 蛋宝
+ * @date: 2024-03-19 22:16
+ * @description: ${Description}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
