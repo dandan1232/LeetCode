69f13e802f5bb5364584537bf15f85a5d2954c8b
dandan1232 <345652127@qq.com>
2024-03-20 17:16:04 +0800

    两数之和

--- src/main/java/org/example/TwoNumberSum.java
+++ src/main/java/org/example/TwoNumberSum.java
@@ -1,40 +1,63 @@
 package org.example;
 
+
+import java.util.Arrays;
+
 /**
  * @author: 蛋宝
  * @date: 2024-03-19 22:22
- * @description:
- * 1.两数之和
+ * @description: 1.两数之和
  * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
  * 你可以按任意顺序返回答案。
- *
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：nums = [2,7,11,15], target = 9
  * 输出：[0,1]
  * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
  * 示例 2：
- *
+ * <p>
  * 输入：nums = [3,2,4], target = 6
  * 输出：[1,2]
  * 示例 3：
- *
+ * <p>
  * 输入：nums = [3,3], target = 6
  * 输出：[0,1]
- *
- *
+ * <p>
+ * <p>
  * 提示：
- *
+ * <p>
  * 2 <= nums.length <= 104
  * -109 <= nums[i] <= 109
  * -109 <= target <= 109
  * 只会存在一个有效答案
- *
- *
+ * <p>
+ * <p>
  * 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
  */
 public class TwoNumberSum {
+
+
+    public static int[] twosum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        String a = Arrays.toString(twosum(nums, target));
+        System.out.println(a);
+    }
 
 
 }
