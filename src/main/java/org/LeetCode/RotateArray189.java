7b8e7c23b06ab12d14c197f1e6246c54c151f6bc
dandan1232 <345652127@qq.com>
2025-02-05 16:25:37 +0800

    189轮转数组

--- src/main/java/org/LeetCode/RotateArray189.java
+++ src/main/java/org/LeetCode/RotateArray189.java
@@ -34,19 +34,20 @@ import java.util.Stack;
  */
 public class RotateArray189 {
     public void rotate(int[] nums, int k) {
-        int n=nums.length;
+        int n= nums.length;
         k=k%n;
-        Stack<Integer> stack=new Stack<>();
-        for (int i = n-k-1; i >=0; i--) {
-            stack.push(nums[i]);
-            System.out.println(stack);
-        }
-        for (int i = n-1; i >=n-k; i--) {
-            stack.push(nums[i]);
-        }
-        System.out.println(stack);
-        for (int i =0; i < n; i++) {
-            nums[i] = stack.pop();
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n-1);
+
+    }
+
+
+    public void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp=nums[i];
+            nums[i++]=nums[j];
+            nums[j--]=temp;
         }
     }
     public static void main(String[] args) {
